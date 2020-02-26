2588779bb9bf2cea439fc864f3146cf1975bcda5
Jakeokony1024 <jakeokony@gmail.com>
2020-02-26 14:35:50 -0600

    cleaned up code slightly

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,12 +2,9 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-    host: "localhost",
-    // Your port; if not 3306
-    port: 3306,
-    // Your username
+    host: "localhost",  
+    port: 3306, 
     user: "root",
-    // Your password
     password: "tomsucks12",
     database: "bamazon_db"
 });
