49dc4c2f43156e448c31d6239ac85f9c84127779
Jakeokony1024 <jakeokony@gmail.com>
2020-02-26 14:34:35 -0600

    got inquirer to run correctly

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Bamazon
+
+inquirer prompt questions: 
+    -which id number are you interested in? 
+    -How many would you like of this item?
+When response is entered, place that into a variable, 
+    if variable is greater than quantity, give insufficient quantity message, 
+    else, add variable to new customer order object containing item, and quantity, and create order id#, 
+    subtract item quantity in db by amount of variable ordered. 
+
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,17 +14,49 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function (err) {
     if (err) throw err;
-    // run the start function after the connection is made to prompt the user
+    // run the function after the connection is made to prompt the user
     afterConnection();
-    connection.end();
+
 });
 
 function afterConnection() {
+
+    console.log("Retrieving Results... ")
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
+
         for (let i = 0; i < res.length; i++) {
-            console.log("ID: #"+ res[i].item_id +"||"+"Item: " + res[i].product_name + "||" +
-                  "Price: $" + res[i].price)
+            console.log("ID: #" + res[i].item_id + "||" + "Item: " + res[i].product_name + "||" +
+                "Price: $" + res[i].price);
         }
-    });
-}
+        inquirer
+            .prompt([{
+                    name: "idAnswer",
+                    type: "rawlist",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < res.length; i++) {
+                            choiceArray.push(res[i].item_id);
+                        }
+                        return choiceArray;
+                    },
+                    message: "What item ID would you like to purchase?"
+                },
+                {
+                    name: "newQuantity",
+                    type: "input",
+                    message: "How many would you like of this item?"
+                },
+
+            ])
+            .then(function (answer) {
+                let newOrder = answer.idAnswer;
+                let orderAmount = answer.newQuantity;
+                console.log("Item ID#: " + newOrder + "  Amount Ordered: " + orderAmount);
+                if 
+            });
+    })
+
+    connection.end();
+
+};
